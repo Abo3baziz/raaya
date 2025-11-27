ddca92194b3a8055c1933d5779b2ae8f71b2a19b
Ahmed Aziz <azez55.ali@gmail.com>
2025-11-27 23:35:08 +0200

    fix: adjust layout and text alignment in PlanCard and Plans components

--- app/ui/PlanCard.tsx
+++ app/ui/PlanCard.tsx
@@ -18,7 +18,7 @@ export function PlanCard({
   benifits,
 }: PlanCardProps) {
   return (
-    <article className="flex flex-col justify-between bg-white rounded-3xl w-fit pt-10 px-10 pb-8 min-w-96 h-150 shadow-lg hover:shadow-2xl transition-shadow duration-300">
+    <article className="flex flex-col justify-between bg-white rounded-3xl pt-10 px-10 pb-8 w-96 h-150 shadow-lg hover:shadow-2xl transition-shadow duration-300">
       <div className="flex flex-col items-end">
         <h2 className="font-semibold text-2xl mb-1.5">{plan_name}</h2>
         <p className="text-paragraph mb-6">{description}</p>
@@ -40,7 +40,7 @@ export function PlanCard({
           <ul className="flex flex-col items-end gap-y-2">
             {benifits.map((benefit, index) => (
               <li key={index} className="flex gap-x-1.5">
-                <span className="text-heading">{benefit}</span>
+                <span className="text-heading text-right">{benefit}</span>
                 <Image src={badge} alt="" width={25} />
               </li>
             ))}
