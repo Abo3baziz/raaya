de7f889e0b60418fdb15ef988efedc15dcbef8c7
Ahmed Aziz <azez55.ali@gmail.com>
2025-11-27 02:50:18 +0200

    add: define metadata for SEO with title, description, keywords, and icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,25 @@
 import "./globals.css";
 import { cairo } from "@/app/ui/fonts";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "شركة رعاية – منصة طبية لحجز المواعيد مع نخبة من الأطباء",
+  description:
+    "شركة رعاية هي منصة طبية موثوقة تتيح لك حجز مواعيد بسهولة مع نخبة من الأطباء في مختلف التخصصات. احجز موعدك الآن واحصل على رعاية صحية موثوقة.",
+  keywords:
+    "شركة رعاية, منصة طبية, حجز مواعيد, أطباء, حجز دكتور, رعاية طبية, خدمات طبية, اطباء متخصصين, استشارات طبية",
+  icons: {
+    icon: "/logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <head>
-        <title>رعاية</title>
-        <link rel="shortcut icon" href="/logo.svg" type="image/x-icon" />
-      </head>
+    <html lang="ar">
       <body className={`${cairo.className} antialiased`}>{children}</body>
     </html>
   );
