bc0267f760a0473ae801b46819715993461dbc0d
Ahmed Aziz <azez55.ali@gmail.com>
2025-11-27 03:12:49 +0200

    refactor: simplify Home component layout by removing unnecessary div wrappers

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,9 @@ import Doctors from "./ui/Doctors";
 export default function Home() {
   return (
     <div>
-      <div className="flex flex-col min-h-screen">
-        <div className="mx-32">
-          <Navbar />
-        </div>
-        <div className="flex-1 grid bg-gold-cream">
-          <Hero />
-        </div>
-      </div>
+      <Navbar />
+
+      <Hero />
 
       <WhoWeAre />
 
