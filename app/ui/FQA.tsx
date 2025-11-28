4c9dd46da9877e57005d66b4818616e59da01fec
Ahmed Aziz <azez55.ali@gmail.com>
2025-11-28 03:30:54 +0200

    fix: add bottom margin to FQA section for improved spacing

--- app/ui/FQA.tsx
+++ app/ui/FQA.tsx
@@ -3,7 +3,7 @@ import { faqData } from "@/app/data/questions";
 
 export default function FQA() {
   return (
-    <section className="bg-main-bg pt-55 px-32">
+    <section className="bg-main-bg pt-55 px-32 mb-40">
       <div className="flex flex-col items-center mb-24">
         <p className="font-semibold text-paragraph mb-5">الاسئله الشائعه</p>
         <h2 className="font-bold text-3xl text-heading mb-3">
