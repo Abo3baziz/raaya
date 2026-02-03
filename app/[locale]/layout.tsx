a50c63df01e8ad1fec08701852b0eedc25acd590
Ahmed Aziz <azez55.ali@gmail.com>
2026-02-03 21:04:21 +0200

    feat: enhance metadata generation and add translations for SEO in layout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -4,17 +4,33 @@ import type { Metadata } from "next";
 import { NextIntlClientProvider, hasLocale } from "next-intl";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
+import { getTranslations } from "next-intl/server";
 
-export const metadata: Metadata = {
-  title: "شركة رعاية – منصة طبية لحجز المواعيد مع نخبة من الأطباء",
-  description:
-    "شركة رعاية هي منصة طبية موثوقة تتيح لك حجز مواعيد بسهولة مع نخبة من الأطباء في مختلف التخصصات. احجز موعدك الآن واحصل على رعاية صحية موثوقة.",
-  keywords:
-    "شركة رعاية, منصة طبية, حجز مواعيد, أطباء, حجز دكتور, رعاية طبية, خدمات طبية, اطباء متخصصين, استشارات طبية",
-  icons: {
-    icon: "/logo.svg",
-  },
-};
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}): Promise<Metadata> {
+  const { locale } = await params;
+
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  const t = await getTranslations({
+    locale,
+    namespace: "meta",
+  });
+
+  return {
+    title: t("title"),
+    description: t("description"),
+    keywords: t("keywords"),
+    icons: {
+      icon: "/logo.svg",
+    },
+  };
+}
 
 export function generateStaticParams() {
   return routing.locales.map((locale) => ({ locale }));
@@ -25,9 +41,10 @@ export default async function RootLayout({
   params,
 }: {
   children: React.ReactNode;
-  params: Promise<{ locale: string }>;
+  params: { locale: string };
 }) {
   const { locale } = await params;
+
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
@@ -35,7 +52,9 @@ export default async function RootLayout({
   return (
     <html lang={locale} dir={locale === "ar" ? "rtl" : "ltr"}>
       <body className={`${cairo.className} antialiased`}>
-        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+        <NextIntlClientProvider locale={locale}>
+          {children}
+        </NextIntlClientProvider>
       </body>
     </html>
   );
